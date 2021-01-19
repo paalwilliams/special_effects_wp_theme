a08bcc446cd731f00bc11c31c247c39a5b89985a
Paal <paalwilliams@gmail.com>
2021-01-19 17:57:50 -0500

    initial commit

--- header.php
+++ header.php
@@ -10,15 +10,24 @@
 <header id="header">
 <div id="branding">
 <div id="site-title">
+</div>
+<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+</div>
+<div id="bottom-header-wrapper">
+    <div id="greeting">
+        <p>GREETINGS, GUEST!</p>
+    </div>
+    <nav id="menu">
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+    </nav>
+    <div id="current-date">
+        <p>TODAY IS <?php echo strtoupper(date('F j\, Y')); ?></p>
+    </div>
+</div>
+<div id="site-title">
 <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
 <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
 </div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu">
-<div id="search"><?php get_search_form(); ?></div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
 </header>
 <div id="container">
